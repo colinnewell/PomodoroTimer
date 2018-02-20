b55df1ddb3b2f1d7d590add6cf61459f900a735e
Colin Newell <colin.newell@gmail.com>
2018-02-20 20:27:07 +0000

    Implemented the threaded version

--- ThreadedPomodoro.cpp
+++ ThreadedPomodoro.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include "ThreadedPomodoro.h"
-#include "timeformatter.h"
 
 using CppLondonUni::ThreadedPomodoro;
+using namespace std::chrono_literals;
 
-std::chrono::high_resolution_clock clk;
+static std::chrono::high_resolution_clock clk;
 
 void ThreadedPomodoro::InvokeProcessLoop(uint32_t time, std::function<bool(uint32_t)>&& callback) {
     // std::thread
@@ -11,12 +12,21 @@ void ThreadedPomodoro::InvokeProcessLoop(uint32_t time, std::function<bool(uint3
     // std::function
     currentTime = time;
     if(runner.joinable()) {
+        done = true;
         runner.join();
     }
     done = false;
-    runner = std::thread([&, callback = std::move(callback)]() mutable {
-        done = callback(currentTime);
-    });
+    runner = std::thread{[&, callback = std::move(callback)]() mutable {
+        while(currentTime > 0 && !done) {
+            auto start = clk.now();
+            done = callback(currentTime--);
+            auto end = clk.now();
+            auto elapsed = end-start;
+            std::this_thread::sleep_for(1s - elapsed);
+        }
+        // also handy for when we reset
+        callback(0); // of by 1 ;)
+    }};
 };
 
 // Interrupt() should result in InvokeProcessLoop returning ASAP
@@ -26,10 +36,9 @@ void ThreadedPomodoro::Interrupt() {
 
 // Same requirements as FormatCurrentTime() in the ISimpleCountdown
 std::string ThreadedPomodoro::FormatCurrentTime() const {
-    timeformatter t;
     auto current = this->currentTime.load();
-    int minutes = current / 60;
-    int seconds = current % 60;
+    auto minutes = current / 60;
+    auto seconds = current % 60;
     return t.FormatTime(minutes, seconds);
 };
 
--- ThreadedPomodoro.h
+++ ThreadedPomodoro.h
@@ -3,12 +3,14 @@
 #include <atomic>
 #include <thread>
 #include "IPomodoro.h"
+#include "timeformatter.h"
 
 using CppLondonUni::IThreadedCountdown;
 
 namespace CppLondonUni {
     class ThreadedPomodoro : public IThreadedCountdown {
 
+    public:
         virtual void InvokeProcessLoop(uint32_t time, std::function<bool(uint32_t)>&& callback);
 
         // Interrupt() should result in InvokeProcessLoop returning ASAP
@@ -23,6 +25,7 @@ namespace CppLondonUni {
         std::atomic_bool done;
         std::atomic_uint_fast32_t currentTime;
         std::thread runner;
+        timeformatter t;
 
     };
 }
